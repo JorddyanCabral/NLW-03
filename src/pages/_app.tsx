f19358a465ca97b37ece5315a1dcaf8d40092436
JorddyanCabral <jorddyan.liceu@gmail.com>
2021-02-26 03:23:05 -0300

    finalizando o dia 3 da NLW#4

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import '../styles/global.css'
+
+import {ChallengesContext, ChallengesProvider} from '../contexts/ChallengesContext'
+import React, { useState } from 'react'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+    <ChallengesProvider>
+
+      <Component {...pageProps} />
+    </ChallengesProvider>
+  
+  )
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,31 @@
 import React from "react";
+
+import Head from 'next/head'
+import styles from '../styles/pages/Home.module.css'
+
 import { CompletedChallenges } from "../components/CompletedChallenges";
+import { ChallengeBox } from "../components/ChallengeBox";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
-import Head from 'next/head'
-import styles from '../styles/pages/Home.module.css'
+
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Início || move.it</title> 
+        <title>Início || move.it</title>
 
       </Head>
       <ExperienceBar />
       <section>
         <div>
           <Profile />
-          <CompletedChallenges/>
-          <Countdown/>
+          <CompletedChallenges />
+          <Countdown />
 
         </div>
         <div>
+          <ChallengeBox />
         </div>
       </section>
     </div>
